bd1520656cd7e7b75270aa720a12c2f74c0bec6c
Krasimir Nedelchev <19822240+kaykayehnn@users.noreply.github.com>
2025-06-18 16:11:06 +0300

    Add day 05

---
+++ day-05/task1.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"strconv"
+	"strings"
+)
+
+func task1(lines []string) {
+
+	allMaps := make([][][]int, 0)
+	for _, el := range lines[1:] {
+		if el == "" {
+			continue
+		}
+		if strings.Contains(el, "map") {
+			allMaps = append(allMaps, make([][]int, 0))
+		} else {
+			nums := strings.Split(el, " ")
+			parsedNums := make([]int, 0)
+			for _, el := range nums {
+				parsed, _ := strconv.Atoi(el)
+				parsedNums = append(parsedNums, parsed)
+			}
+			allMaps[len(allMaps)-1] = append(allMaps[len(allMaps)-1], parsedNums)
+		}
+	}
+
+	seeds := strings.Split(strings.ReplaceAll(lines[0], "seeds: ", ""), " ")
+	parsedSeeds := make([]int, 0)
+	for _, el := range seeds {
+		parsed, _ := strconv.Atoi(el)
+		parsedSeeds = append(parsedSeeds, parsed)
+	}
+
+	lowestLocation := math.MaxInt64
+	for _, el := range parsedSeeds {
+		mappedValue := el
+		for _, myMap := range allMaps {
+			for _, mapping := range myMap {
+				destination := mapping[0]
+				start := mapping[1]
+				length := mapping[2]
+				if mappedValue >= start && mappedValue < start+length {
+					offset := mappedValue - start
+					mappedValue = destination + offset
+					break
+				}
+			}
+		}
+
+		lowestLocation = min(lowestLocation, mappedValue)
+	}
+	fmt.Println(lowestLocation)
+}
